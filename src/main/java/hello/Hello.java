9e856f62274897ca0aa3cb91ec67cee880b9be1d
Alexey Afanasiev <megabochka@gmail.com>
2023-11-20 21:38:40 +0100

    Update Hello.java

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println(new String("message"));
         if (true) {
             unknown();
         }
