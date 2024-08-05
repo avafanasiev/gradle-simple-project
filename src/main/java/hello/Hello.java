9f5913943d23d7038a72c56882b5ec183d07d092
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2024-08-06 00:08:53 +0200

    Add velocity to imports

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,11 +1,17 @@
 package hello;
 
+import org.apache.velocity.app.Velocity;
+
 import java.util.stream.Stream;
 
 
 public class Hello {
 
     public static void main(String[] args) {
+
+        Velocity.init();
+
+        String v = "";
         if (true) {
 
         }
